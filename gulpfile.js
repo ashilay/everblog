4e141e132f99eccc8bed459c34000beeaef06d21
Artem Shylai <artemshilay@gmail.com>
2015-01-14 18:28:41 +0200

    cleanup and vulcanization with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 var gulp = require('gulp'),
 	vulcanize = require('gulp-vulcanize');
 
+var del = require('del');
 
-gulp.task('default', function () {
+
+gulp.task('vulcanize-csp', function () {
     var DEST_DIR = 'dist';
 
     return gulp.src('index.html')
         .pipe(vulcanize({
             dest: DEST_DIR,
-            strip: true
+            strip: true,
+            csp: true
         }))
         .pipe(gulp.dest(DEST_DIR));
+});
+
+gulp.task('clean', function(cb) {
+    del(['dist/'], cb)
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('vulcanize-csp');
 });
